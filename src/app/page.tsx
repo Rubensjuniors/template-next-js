1c0a68d24af9350db56f302c320b9a2800e6db0d
rubens.marques <rubens.marques@hotmart.com>
2024-08-08 10:15:32 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   const teste = ''
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>dddd</h2>
+      <h2>ddddnhhjjhjhjh</h2>
     </main>
   )
 }
